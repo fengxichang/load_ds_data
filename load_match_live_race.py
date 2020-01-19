2fc73819ad88bb9442e638155c050e1618c8cdbf
fengxichang <1058991287@qq.com>
2020-01-19 21:44:16 +0800

    load 实时赔率数据优化

--- load_match_live_race.py
+++ load_match_live_race.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-# Created on 2020-01-1 19:52:55
+# Created on 2020-01-18 19:52:55
 # Project: load_match_live_race
 
 from pyspider.libs.base_handler import *
@@ -63,13 +63,25 @@ def get_headers():
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0,image/webp,*/*;q=0.8",
         "Accept-Encoding": "gzip, deflate, sdch, br",
         "Accept-Language": "zh-CN, zh; q=0.8, en; q=0.6",
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',  
     }
+
+def getCookie():
+    return {
+        "uid": "R-563407-3387d81805e23c6c2f1e24",
+        "ds_session": "b2p49bqf6db0ds064k64f4pl85",
+        "halfbt_daxiao": "1",
+        "halfbt_rangfen": "1",
+        "race_id": "561883",
+        #"Hm_lvt_a68414d98536efc52eeb879f984d8923": "1579348840,1579402940",
+        #"Hm_lpvt_a68414d98536efc52eeb879f984d8923": "1579403944"
+    }
+    
 
 
 class Handler(BaseHandler):
     crawl_config = {
-        'proxy':"HZ66310Z7I39EY4P:1CCD8C4876D5635C@http-pro.abuyun.com:9010"
+        'proxy':"HZ66310Z7I39EY4P:1CCD8C4876D5635C@http-pro.abuyun.com:9010",
     }
     
     
@@ -79,14 +91,14 @@ class Handler(BaseHandler):
     
     def index_page(self, response):
         start = "2019-01-01"
-        end = "2019-06-30"
+        end = "2019-01-01"
         
-        match_list_res = requests.get("http://local.ds.football/api/match/getMatchByDate?startDate="+start+"&endDate="+endDate)
+        match_list_res = requests.get("http://local.ds.football/api/match/getMatchByDate?startDate="+start+"&endDate="+end)
 
         match_list = json.loads(match_list_res.content)
 
         for match_id in match_list:
-            self.crawl("https://www.dszuqiu.com/race_sp/"+match_id, callback=self.detail_page, validate_cert=False, headers=get_headers(), cookies=random_bid())
+            self.crawl("https://www.dszuqiu.com/race_sp/"+str(match_id), callback=self.detail_page, validate_cert=False, headers=get_headers(), cookies=getCookie())
   
     @config(priority=2)
     def detail_page(self, response):
@@ -94,24 +106,29 @@ class Handler(BaseHandler):
         bs_full_data = []
         wd_halt_data = []
         wd_full_data = []
+        
+        print(response.doc)
+        
+        match_id = response.url[8:].split('/')[-1]
 
         #大小球半场
         for tr in response.doc("div[class='small-4 columns tableCol3in1 daxiao']").find("table[class='responsive half'] > tbody").items("tr"):
-            match_id = response.url[8:].split('/')[-1]
-            time = tr.children("td").eq(0).text()[:-1]
+            match_time = tr.children("td").eq(0).text()[:-1]
             score = tr.children("td").eq(1).text()
             big = tr.children("td").eq(2).text()
-            small = tr.children("td").eq(3).text()
-            bsp = tr.children("td").eq(4).text()
+            bsp = tr.children("td").eq(3).text()
+            small = tr.children("td").eq(4).text()
+            date_time = tr.children("td").eq(5).text()
             _type = 0
             
             live_data = {
                 "match_id": match_id,
-                "time": time,
+                "match_time": match_time,
                 "score": score,
                 "big": big,
                 "small": small,
                 "bsp": bsp,
+                "date_time": date_time,
                 "type": _type 
             }
             
@@ -119,45 +136,45 @@ class Handler(BaseHandler):
 
         #大小球全场
         for tr in response.doc("div[class='small-4 columns tableCol3in1 daxiao']").find("table[class='responsive full'] > tbody").items("tr"):
-            match_id = response.url[8:].split('/')[-1]
-            time = tr.children("td").eq(0).text()[:-1]
+            match_time = tr.children("td").eq(0).text()[:-1]
             score = tr.children("td").eq(1).text()
             big = tr.children("td").eq(2).text()
-            small = tr.children("td").eq(3).text()
-            bsp = tr.children("td").eq(4).text()
+            bsp = tr.children("td").eq(3).text()
+            small = tr.children("td").eq(4).text()
+            date_time = tr.children("td").eq(5).text()
             _type = 1
             
             live_data = {
                 "match_id": match_id,
-                "time": time,
+                "match_time": match_time,
                 "score": score,
                 "big": big,
                 "small": small,
                 "bsp": bsp,
+                "date_time": date_time,
                 "type": _type 
             }
             
             bs_full_data.append(live_data)
 
-
-
         #胜平负半场
         for tr in response.doc("div[class='small-4 columns tableCol3in1 wincast']").find("table[class='responsive half'] > tbody").items("tr"):
-            match_id = response.url[8:].split('/')[-1]
-            time = tr.children("td").eq(0).text()[:-1]
+            match_time = tr.children("td").eq(0).text()[:-1]
             score = tr.children("td").eq(1).text()
             win = tr.children("td").eq(2).text()
-            draw = tr.children("td").eq(3).text()
+            let_ball = tr.children("td").eq(3).text()
             lose = tr.children("td").eq(4).text()
+            date_time = tr.children("td").eq(5).text()
             _type = 0
             
             live_data = {
                 "match_id": match_id,
-                "time": time,
+                "match_time": match_time,
                 "score": score,
-                "win": big,
-                "draw": small,
-                "lose": bsp,
+                "win": win,
+                "let_ball": let_ball,
+                "lose": lose,
+                "date_time": date_time,
                 "type": _type 
             }
             
@@ -166,27 +183,39 @@ class Handler(BaseHandler):
 
         #胜平负全场
         for tr in response.doc("div[class='small-4 columns tableCol3in1 wincast']").find("table[class='responsive full'] > tbody").items("tr"):
-            match_id = response.url[8:].split('/')[-1]
-            time = tr.children("td").eq(0).text()[:-1]
+            match_time = tr.children("td").eq(0).text()[:-1]
             score = tr.children("td").eq(1).text()
             win = tr.children("td").eq(2).text()
-            draw = tr.children("td").eq(3).text()
+            let_ball = tr.children("td").eq(3).text()
             lose = tr.children("td").eq(4).text()
+            date_time = tr.children("td").eq(5).text()
             _type = 1
             
             live_data = {
                 "match_id": match_id,
-                "time": time,
+                "match_time": match_time,
                 "score": score,
-                "win": big,
-                "draw": small,
-                "lose": bsp,
+                "win": win,
+                "let_ball": let_ball,
+                "lose": lose,
+                "date_time": date_time,
                 "type": _type 
             }
             
-            wd_full_data.append(live_data)    
+            wd_full_data.append(live_data)
 
-           
-        return { match_date: json.loads(insert_res.content) }
+        #要保存的数据
+        post_data = {
+            "bs_halt_data": json.dumps(bs_halt_data),
+            "bs_full_data": json.dumps(bs_full_data),
+            "wd_halt_data": json.dumps(wd_halt_data),
+            "wd_full_data": json.dumps(wd_full_data)
+        }
+        
+        print(post_data)
+        
+        insert_res = requests.post("http://local.ds.football/api/match/live/store", data=post_data)    
+
+        return { match_id: json.loads(insert_res.content) }
             
         
